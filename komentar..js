5d318c253ae802466837fcca3128f6180249b616
nknnzw <127169952+nknnzw@users.noreply.github.com>
2023-03-27 14:32:44 +0700

    komentar

---
+++ komentar..js
@@ -0,0 +1,4 @@
+// ini merupakan komentar satu baris,kode di bawah tidak dijalankan
+// console.log("Halo!");
+
+/* ini merupakan komentar dengan lebih dari satu baris teks apapun yang berada disini akan dijadikan komentar. ketika menggunakan ini,jangan lupa untuk menutupnya*/
